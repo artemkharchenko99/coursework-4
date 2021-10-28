ba2e1ed44d7329024d736d3bbd8bfc35824189dc
artemij <artyomkharchenko99@gmail.com>
2021-10-28 12:30:29 +0300

    commit

---
+++ src/components/StudentInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>{{ student.name }}</p>
+        <p>{{ student.group }}</p>
+        <p>{{ "Оценка: " + student.mark }}</p>
+        <img :src="student.photo">
+    </div>
+</template>
+<script>
+    import Vue from 'vue'
+    import axios from 'axios'
+
+    export default {
+        props: {
+            id: '',
+        },
+        data: function() {
+            return {
+                student: {},
+            };
+        },
+    mounted: function(){
+    axios.get("http://46.101.212.195:3000/students/"+this.id).then((response) => {
+        console.log(response.data)
+        this.student = response.data;
+        })
+    },
+}
+</script>
